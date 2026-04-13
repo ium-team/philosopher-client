4a2dd68db0d78cf5b8e7578f211704e2f5d06c4b
hefxpzwk <hefxpzwk@gmail.com>
2026-04-13 19:04:55 +0900

    feat(service): open philosopher selection by default on /service

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -1,5 +1,5 @@
 import { ServicePage } from "@/features/service/components/service-page";
 
 export default function ServiceRoute() {
-  return <ServicePage />;
+  return <ServicePage startInSelection />;
 }
