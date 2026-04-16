88c4cc39b994f41ca4bda3bbc8175c8aa2ef6362
hefxpzwk <hefxpzwk@gmail.com>
2026-04-16 13:04:34 +0900

    feat: add Vercel Analytics to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { JetBrains_Mono, Noto_Sans_KR } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import { siteConfig } from "@/config/site";
 import "./globals.css";
 
@@ -32,7 +33,10 @@ export default function RootLayout({
       lang="ko"
       className={`${sans.variable} ${mono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
